fce5b7b23eaab588432e6d7163de24d952fd308b
Engraya <ahmadyakubuaya@gmail.com>
2024-08-07 19:23:25 +0100

    Fix Id Issue

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -27,71 +27,68 @@ interface PromptCardProps {
   handleDelete?: () => void;
   handleTagClick?: (tag: string) => void;
 }
+
 const PromptCard: React.FC<PromptCardProps> = ({ post, handleEdit, handleDelete, handleTagClick }) => {
-    const { data: session } = useSession();
-    const pathName = usePathname();
-    const router = useRouter();
-  
-    const [copied, setCopied] = useState<string>("");
-  
-    const handleProfileClick = () => {
-                // @ts-ignore
-      if (post.creator._id === session?.user?.id) return router.push("/profile");
-  
-      router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
-    };
-  
-    const handleCopy = () => {
-      setCopied(post.prompt);
-      navigator.clipboard.writeText(post.prompt);
-      setTimeout(() => setCopied(""), 3000);
-    };
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState<string>("");
+
+  const handleProfileClick = () => {
+    if (post.creator._id === session?.user?.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  };
+
   return (
     <div 
-    onClick={handleProfileClick}
-    className="aspect-auto p-6 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none">
-    <div className="flex justify-between items-center gap-2">
-      <Image
-      className="w-12 h-12 rounded-full"
-      src={post.creator.image}
-      alt="user avatar"
-      width={400}
-      height={400}
-      loading="lazy"
-       />
-      <div>
-        <h6 className="text-lg font-medium text-gray-700 dark:text-white">{post.creator.username}</h6>
-        <p className="text-sm text-gray-500 dark:text-gray-300">{post.creator.email}</p>
+      onClick={handleProfileClick}
+      className="aspect-auto p-6 border border-gray-100 rounded-3xl bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 dark:shadow-none"
+    >
+      <div className="flex justify-between items-center gap-2">
+        <Image
+          className="w-12 h-12 rounded-full"
+          src={post.creator.image}
+          alt="user avatar"
+          width={400}
+          height={400}
+          loading="lazy"
+        />
+        <div>
+          <h6 className="text-lg font-medium text-gray-700 dark:text-white">{post.creator.username}</h6>
+          <p className="text-sm text-gray-500 dark:text-gray-300">{post.creator.email}</p>
+        </div>
+        <div onClick={handleCopy} className='p-4 ml-auto rounded-full bg-gray-200 cursor-pointer overflow-hidden'>
+          { copied === post.prompt ? <IoCheckmarkDoneSharp className='text-blue-500 hover:text-blue-600'/> : <IoIosCopy className='text-blue-500 hover:text-blue-600'/> }
+        </div>
       </div>
-      <div  onClick={handleCopy} className='p-4 ml-auto rounded-full bg-gray-200 cursor-pointer overflow-hidden'>
-      { copied === post.prompt ? <IoCheckmarkDoneSharp className='text-blue-500 hover:text-blue-600'/> : <IoIosCopy className='text-blue-500 hover:text-blue-600'/> }
+      <p className="mt-8 text-teal-950 dark:text-gray-200">{post.prompt}</p>
+      <div onClick={() => handleTagClick && handleTagClick(post.tag)} className='text-blue-500 hover:text-blue-600 mt-4'>
+        #{post.tag}
       </div>
-    </div>
-    <p className="mt-8 text-teal-950 dark:text-gray-200">{post.prompt}</p>
-    <div onClick={() => handleTagClick && handleTagClick(post.tag)} className='text-blue-500 hover:text-blue-600 mt-4'>
-    #{post.tag}
-  </div>
-  <>
-  
-  {session?.user?.id === post.creator._id && pathName === "/profile" && (
+      {session?.user?.id === post.creator._id && pathName === "/profile" && (
         <div className="flex space-x-4 justify-center items-center mt-3 flex-center gap-4 border-t border-gray-100 pt-2">
-        {/* <button 
+          {/* <button 
             onClick={handleEdit}
             className="border border-green-500 text-green-500 font-bold py-1 px-2 rounded-lg">
             Edit
-        </button> */}
-        <button 
+          </button> */}
+          <button 
             onClick={handleDelete}
             className="border border-red-500 text-red-500 font-bold py-1 px-2 rounded-lg">
             Delete
-        </button>
+          </button>
         </div>
       )}
-  </>
-
-
-  </div>
-  )
+    </div>
+  );
 }
 
-export default PromptCard
+export default PromptCard;
